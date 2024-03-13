dac58f65744a9e7ad26eb65ba99ffbd8d1ec3a53
keemunji <kej20211496@gmail.com>
2024-03-13 23:56:27 +0900

    Refactor :  불변의 객체 State로 업데이트

--- 04 Essentials Deep Dive/src/App.jsx
+++ 04 Essentials Deep Dive/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
   return <main>
@@ -7,7 +8,7 @@ function App() {
         <Player initalName="Player 1" symbol="X"/>
         <Player initalName="Player 2" symbol="O"/>
       </ol>
-      GAME BOARD
+      <GameBoard />
     </div>
 
     LOG
