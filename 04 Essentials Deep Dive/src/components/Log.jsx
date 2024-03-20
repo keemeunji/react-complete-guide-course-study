2fa69a31178058b67c825bbbd3b30fa365764e6f
keemunji <kej20211496@gmail.com>
2024-03-20 23:58:26 +0900

    Refactor : 조건적 버튼 비활성화

--- 04 Essentials Deep Dive/src/components/Log.jsx
+++ 04 Essentials Deep Dive/src/components/Log.jsx
@@ -1,7 +1,7 @@
 export default function Log({ turns }) {
     return (
         <ol id="log">
-            {turns.map((turn => (
+            {turns.map((turn) => (
                 <li key={`${turn.square.row}${turn.square.col}`}>
                     {turn.player} selected {turn.square.row},{turn.square.col}
                 </li>
